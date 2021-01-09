92f8fbf1c2d45a852f188a1d77c0dc7affac79a2
Eduardo <jorge.ecrg@gmail.com>
2021-01-09 15:17:55 -0400

    add response perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response;
 
 class PerfilController extends Controller
 {
