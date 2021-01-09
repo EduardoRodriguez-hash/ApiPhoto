0260566ba85cfe5f192fbe34a452e9316edeecc9
Eduardo <jorge.ecrg@gmail.com>
2021-01-09 17:47:25 -0400

    Se soluciono route

--- app/Http/Controllers/FotoController.php
+++ app/Http/Controllers/FotoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Response;
 
 class FotoController extends Controller
 {
