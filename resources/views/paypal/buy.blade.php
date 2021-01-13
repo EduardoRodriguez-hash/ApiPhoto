e2cd7d98710625b8c9595c433b097b13bde47617
Eduardo <jorge.ecrg@gmail.com>
2021-01-13 19:47:42 -0400

    Solucion compra

--- resources/views/paypal/buy.blade.php
+++ resources/views/paypal/buy.blade.php
@@ -6,16 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paypal</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="{{asset('./css/styles.css')}}" />
-
     <!-- BootStrap 4 CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <!-- Bootstrap 4 JQUERY -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
 
@@ -36,12 +28,6 @@
                 <div class="card">
 
                     <div class="card-header bg-white">
-
-                        <div class="pos-rel img-tam mx-auto">
-                            <img src="{{route('getImagePhoto',['filename'=>$foto->filename])}}" alt="No se pudo cargar la imagen" class="img-fluid rounded border opacity" id="NotDowload" . />
-                            <h2 class="p-abs text-white text-center">Comprar Foto</h2>
-                        </div>
-
                     </div>
 
                     <div class="card-body">
@@ -77,8 +63,6 @@
 
         </div>
     </div>
-
-    <script src="{{asset('./js/all.js')}}"></script>
 
 </body>
 
